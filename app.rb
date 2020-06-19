b1c91b8f6a12266550e09120698bbac28df414a9
Irene Scott <irenejinscott@gmail.com>
2020-06-19 05:38:00 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
   end
 
   post '/piglatinize' do
-   @analyzed_text = PigLatinizer.new.piglatinize(params["user_phrase"])
+   @text = PigLatinizer.new.piglatinize(params["user_phrase"])
   erb :results
   end
 
