88d2d762bf7b049816ab8f1f2de0f232ef7e54c7
Irene Scott <irenejinscott@gmail.com>
2020-06-19 05:33:12 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,7 +1,7 @@
 class PigLatinizer
 
-  def piglatinize(phrase)
-    @phrase = phrase
+  def piglatinize(user_phrase)
+    @phrase = user_phrase
     string = @phrase
     words = string.split(" ")
     said_phrase = words.collect  {|word| piglatinize_the_word(word)}
